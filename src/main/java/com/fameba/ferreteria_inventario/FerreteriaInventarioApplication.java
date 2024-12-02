a47e13f5bc3173f94552377b1185848ce3631459
Jhon <mc.maicks@gmail.com>
2024-12-02 10:46:25 -0400

    Initial commit

---
+++ src/main/java/com/fameba/ferreteria_inventario/FerreteriaInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.fameba.ferreteria_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaInventarioApplication.class, args);
+	}
+
+}
